7bacc111bdd1d14d46d5faa6e7cfa6f12e589adf
james <jjbe.93@gmail.com>
2020-04-15 23:27:17 +0300

    added getLatest func on home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,24 @@
 import React from "react";
+const axios = require('axios');
 
 const Home = (props) => {
-  return <h1>HOME</h1>;
+
+  const getLatestMovies = () =>{
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.tmdb_api_key}&language=en-US&page=1`);
+      console.log(response)
+    } catch (error) {
+      
+    }
+  };
+
+  return (<>
+    <h1>HOME</h1>
+    <button onClick={getLatestMovies}></button>
+  
+  
+  
+  </>);
 };
 
 export default Home;
