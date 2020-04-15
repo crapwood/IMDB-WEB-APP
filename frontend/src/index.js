add779e487dd46c90d7dab40619771ad0300c970
james <jjbe.93@gmail.com>
2020-04-15 23:18:25 +0300

    added dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+require("dotenv").config();
 
 // const store = createStore(allReducers);
 
