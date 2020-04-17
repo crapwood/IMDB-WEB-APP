39c873c6e92ba1a4264d3d0d6f5b6cd18299c58c
james <jjbe.93@gmail.com>
2020-04-17 21:29:16 +0300

    fixing api call

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
       const response = await axios.get(
         `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
       );
-      console.log(response);
+      console.log(response.data.results);
     } catch (error) {}
   };
 
