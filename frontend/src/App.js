4f0ce2ecfe75a957ec78a37d0e38da8db0879e34
james <jjbe.93@gmail.com>
2020-04-17 23:08:47 +0300

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Nav from "./pages/navbar";
 
 // PAGES
 import Home from "./pages/Home/home.js";
+import Movies from "./pages/Movies/movies.js";
+import TvShows from "./pages/TV_Shows/tv.js";
+import Search from "./pages/Adv_Search/search.js";
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <div>
           <Nav />
           <Switch>
-            <Route exactly component={Home} pattern="/" />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/movies" component={Movies} />
+            <Route exact path="/tvshows" component={TvShows} />
+            <Route exact path="/advance-search" component={Search} />
           </Switch>
         </div>
       </Router>
