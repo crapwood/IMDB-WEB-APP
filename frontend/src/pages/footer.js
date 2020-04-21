b9511fad3300cfa9101b2b93f19f2b285fa7597c
james <jjbe.93@gmail.com>
2020-04-21 23:40:11 +0300

    fixing error

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Footer = (props) => {
-  return;
-  <>
-    <p>All rights reserved @ James Benitez</p>
-    <a href="https://github.com">github</a>
-  </>;
+  return (
+    <>
+      <p>All rights reserved @ James Benitez</p>
+      <a href="https://github.com">github</a>
+    </>
+  );
 };
 
 export default Footer;
