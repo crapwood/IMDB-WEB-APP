86d76c46ac35eb075692f0f46ccbaa1bdb91fad7
james <jjbe.93@gmail.com>
2020-04-21 23:39:06 +0300

    added tabs on footer

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = (props) => {
-  return <p>All rights reserved @ James Benitez</p>;
+  return;
+  <>
+    <p>All rights reserved @ James Benitez</p>
+    <a href="https://github.com">github</a>
+  </>;
 };
 
 export default Footer;
