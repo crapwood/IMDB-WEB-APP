d3d4105e4dd9cb31c191a0e4995720f7e2b22588
james <jjbe.93@gmail.com>
2020-04-22 23:43:30 +0300

    added container on nav

--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -2,25 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Container from "@material-ui/core/Container";
 
 const NavBar = (props) => {
   return (
     <>
-      <ButtonGroup color="primary" aria-label="outlined primary button group">
-        <Button>
-          <Link to="/">Home</Link>
-        </Button>
-        <Button>
-          <Link to="/movies">MOVIES</Link>
-        </Button>
-        <Button>
-          <Link to="/tvshows">TV SHOWS</Link>
-        </Button>
-        <Button>
-          <Link to="/advanced-search">ADVANCED SEARCH</Link>
-        </Button>
-      </ButtonGroup>
-      <input type="text" placeholder="Search.."></input>
+      <Container maxWidth="sm">
+        <ButtonGroup color="primary" aria-label="outlined primary button group">
+          <Button>
+            <Link to="/">Home</Link>
+          </Button>
+          <Button>
+            <Link to="/movies">MOVIES</Link>
+          </Button>
+          <Button>
+            <Link to="/tvshows">TV SHOWS</Link>
+          </Button>
+          <Button>
+            <Link to="/advanced-search">ADVANCED SEARCH</Link>
+          </Button>
+        </ButtonGroup>
+      </Container>
     </>
   );
 };
