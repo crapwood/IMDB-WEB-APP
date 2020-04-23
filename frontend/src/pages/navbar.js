bccb573b049203858487ee50875102edd604fcdc
james <jjbe.93@gmail.com>
2020-04-23 23:21:21 +0300

    change navbar

--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Container from "@material-ui/core/Container";
+
 import "./navbar.css";
 
 const NavBar = (props) => {
   return (
     <>
-      <section className="nav-container">
-        <ButtonGroup aria-label="outlined primary button group">
+      {/* <section className="nav-container"> */}
+      {/* <ButtonGroup aria-label="outlined primary button group">
           <Button variant="outlined">
             <Link className="nav-link" to="/">
               Home
@@ -30,8 +28,89 @@ const NavBar = (props) => {
               ADVANCED SEARCH
             </Link>
           </Button>
-        </ButtonGroup>
-      </section>
+        </ButtonGroup> */}
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand nav-link" to="/">
+          Home
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              {/* <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a> */}
+              <Link className="nav-link" to="/">
+                Home <span class="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      {/* </section> */}
       {/* <hr /> */}
     </>
   );
