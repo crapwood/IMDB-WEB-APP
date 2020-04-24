de01a5e3cf0c5b79922de7517c4b1cb36cc40782
james <jjbe.93@gmail.com>
2020-04-24 17:37:45 +0300

    added icon

--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -7,9 +7,10 @@ const NavBar = (props) => {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand nav-link" to="/">
+        {/* <Link className="navbar-brand nav-link" to="/">
           Home
-        </Link>
+        </Link> */}
+        <i class="fas fa-film fa-3x"></i>
         <button
           class="navbar-toggler"
           type="button"
@@ -70,21 +71,8 @@ const NavBar = (props) => {
               </div>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </nav>
-      {/* </section> */}
-      {/* <hr /> */}
     </>
   );
 };
