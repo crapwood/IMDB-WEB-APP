f5cb32226f5e54565ba646fa8c91966e112fd5bc
james <jjbe.93@gmail.com>
2020-04-24 19:26:52 +0300

    added latest movies on home

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 const axios = require("axios");
 
 const Home = (props) => {
+  const [moviesOnTheater, setMoviesOnTheater] = useState([]);
+
   const getLatestMovies = async () => {
     try {
       const response = await axios.get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=7d37867c5ca54dec639654f44aa6d8ab&language=en-US`
       );
       console.log(response.data.results);
+      setMoviesOnTheater([...response.data.results]);
     } catch (error) {}
+  };
+
+  const printData = () => {
+    return moviesOnTheater.map((movie) => (
+      <>
+        <img
+          src={" http://image.tmdb.org/t/p/w185/" + movie.poster_path}
+          alt="..."
+          class="img-thumbnail"
+        ></img>
+        <p>{movie.title}</p>
+      </>
+    ));
   };
 
   return (
     <>
-      <h1>HOME</h1>
-      <p>TODO: put grid here of thumbnails of movies poster</p>
-      <p>Add ratings from omdb</p>
-      <button onClick={getLatestMovies}>Click!!!</button>
+      <div class="container-fluid">
+        <button onClick={getLatestMovies}>Click!!!</button>
+        <hr />
+        {printData()}
+      </div>
     </>
   );
 };
